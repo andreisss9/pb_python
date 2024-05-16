9d99d0f59dcc885d5ad437e5bc096de44623ce7b
andreisss9 <andressa.reis90@gmail.com>
2024-05-16 16:50:36 -0300

    versão completa

--- hello.py
+++ hello.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+"""Hello Multi Línguas.
 
-#Este programa fala com você
-print ("Hello")
+Dependendo da língua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar: 
+
+Tenha a variável LANG devidamente configurada ex: 
+
+    export LANG=pt_BR
+
+Execução:
+    python 3 hello.py
+    ou 
+    ./hello.py
+"""
+
+#Dunders
+__version__="0.0.1"
+__author__="Andressa Reis"
+__license__="Unlicense"
+
+import os
+
+#Este programa imprime uma mensagem
+
+current_language = os.getenv("lang", "en_US")[:5]
+msg= "Hello"
+
+if current_language == "pt_BR":
+    msg= "Olá"
+elif current_language == "it_IT":
+    msg = "Ciao"
+
+print (msg)
